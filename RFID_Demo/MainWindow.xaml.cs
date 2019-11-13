a29d9a2910df8652a7feaeb01d540d39c0dc8a6b
Carlos Perez <carlosperez1001@gmail.com>
2019-11-13 12:03:02 -0500

    Add project files.

---
+++ RFID_Demo/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ThingMagic;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+using DataGrid = System.Windows.Controls.DataGrid;
+using System.Collections.ObjectModel;
+
+namespace RFID_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        public string EPC { get; private set; }
+        public string timeStamp { get; private set; }
+        public string RSSI { get; private set; }
+
+        public int COM_Port = 0;
+        public bool connectiveStatus = false;
+        public ThingMagic.Reader reader;
+        ObservableCollection<UnknownRFID> UnknownRFIDList = new ObservableCollection<UnknownRFID>();
+        
+
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+
+            UnregisteredDataGrid.ItemsSource = UnknownRFIDList;
+            updateConnectiveStatus();
+
+
+        }
+
+        public void updateConnectiveStatus() {
+            if (connectiveStatus == true) {
+                Icon_ConnectiveStatus.Fill = new SolidColorBrush(Colors.Yellow);
+                cbox_COM.IsEnabled = false;
+                btn_Connect.IsEnabled = false;
+                lbl_ConnectiveStatus.Content = "Idle";
+            }
+            else
+            {
+                Icon_ConnectiveStatus.Fill = new SolidColorBrush(Colors.Red);
+                lbl_ConnectiveStatus.Content = "Disconnected";
+            }
+        }
+
+        public void ConnectRFID()
+        {
+            {
+                //Configurations of COMs "USB Port"
+
+                //Attempt Connection
+                try
+                {
+                    Console.WriteLine(COM_Port);
+                    string uri = "eapi:///com" + COM_Port;
+                    reader = ThingMagic.Reader.Create(uri);
+
+                    reader.Connect();
+                    reader.ParamSet("/reader/region/id", ThingMagic.Reader.Region.NA);
+                    //Select Antenna 1 port manually 
+                    int[] antennaList = null;
+                    string str = "1,1";
+                    antennaList = Array.ConvertAll(str.Split(','), int.Parse);
+                    SimpleReadPlan plan = new SimpleReadPlan(antennaList, TagProtocol.GEN2, null, null, 1000);
+                    reader.ParamSet("/reader/read/plan", plan);
+                    connectiveStatus = true;
+                    updateConnectiveStatus();
+
+
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    connectiveStatus = false;
+                    //  updateConnectiveStatus();
+                    MessageBox.Show("COM " + COM_Port + " is not valid. ");
+
+
+                }
+            }
+        }
+
+
+        public void ReadBatchRFID()
+        {
+            if (connectiveStatus == true)
+            {
+                try
+                {
+
+                    int timeout = 1000;
+
+
+                    float rssiPower = 0;
+                    reader.StartReading();
+                    reader.TagRead += OnTagRead;
+
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("M6e is not connected properly. Please select the correct COM port that connects the M6e module");
+            }
+        }
+
+        private void OnTagRead(Object sender, TagReadDataEventArgs e) {
+            bool existingTag = false;
+            Console.WriteLine(e.TagReadData.EpcString + " " + e.TagReadData.Time.ToString() + " " + e.TagReadData.Rssi.ToString());
+            
+            if (UnknownRFIDList.Any(p => p.EPC == e.TagReadData.EpcString)) {
+                var file = UnknownRFIDList.First(f => f.EPC == e.TagReadData.EpcString) ;
+                var index = UnknownRFIDList.IndexOf(file);
+                UnknownRFIDList[index].timeStamp = e.TagReadData.Time.ToString();
+                UnknownRFIDList[index].RSSI = e.TagReadData.Rssi.ToString();
+                Console.WriteLine("I have " + e.TagReadData.EpcString);
+                this.Dispatcher.Invoke(() =>
+                {
+                    UnregisteredDataGrid.Items.Refresh();
+                });
+                }
+            else {
+                this.Dispatcher.Invoke(() =>
+                {
+                    UnknownRFIDList.Add(new UnknownRFID(EPC = e.TagReadData.EpcString, timeStamp = e.TagReadData.Time.ToString(), RSSI = e.TagReadData.Rssi.ToString()));
+                });
+            }
+        }
+    
+        
+
+        private void btn_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+           ReadBatchRFID();
+        }
+
+
+        private void onClick_btnConnect(object sender, RoutedEventArgs e)
+        {
+
+            COM_Port = cbox_COM.SelectedIndex + 1;
+      
+            Console.WriteLine(COM_Port);
+
+             ConnectRFID();
+
+        }
+
+        private void btn_AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Check List");
+            for (int i = 0; i < UnknownRFIDList.Count; i++)
+            {
+                Console.WriteLine(i+ "] " + string.Concat(UnknownRFIDList[i].EPC, "---", UnknownRFIDList[i].timeStamp));
+            }
+        }
+    }
+}
+
+    
---
+++ RFID_Demo/UnknownRFID.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFID_Demo
+{
+
+   
+    public class UnknownRFID
+    {
+        public string _EPC;
+        public string _timeStamp;
+        public string _RSSI;
+
+        public UnknownRFID(String EPC, String timeStamp, String RSSI)
+        {
+            this.RSSI = RSSI;
+            this.EPC = EPC;
+            this.timeStamp = timeStamp; //RawTagData.Substring(44);
+            
+
+        }
+        public string EPC
+        { 
+            get { return _EPC; }
+            set { _EPC = value; }
+           
+        }
+        public string timeStamp {
+            get { return _timeStamp; }
+            set { _timeStamp = value; }
+           
+        }
+        public string RSSI
+        {
+            get { return _RSSI; }
+            set { _RSSI = value; }
+         
+        }
+    public void printRFID()
+        {
+          //  Console.WriteLine(this.EPC + "   " + this.timeStamp);
+        }
+
+       
+    }
+
+
+}
+

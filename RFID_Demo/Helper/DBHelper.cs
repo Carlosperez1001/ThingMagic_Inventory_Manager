030814bca9bf92c4ba1b383df05ccb80efad0ed9
Carlos Perez <carlosperez1001@gmail.com>
2019-11-19 17:13:23 -0500

    Fix disconnected DB event

--- RFID_Demo/Helper/DBHelper.cs
+++ RFID_Demo/Helper/DBHelper.cs
@@ -12,13 +12,16 @@ namespace RFID_Demo
 
     public static class DBHelper
     {
-        
+
         private static MySqlConnection connection;
         private static MySqlCommand cmd = null;
         private static MySqlDataAdapter da;
         public static DataTable dt = new DataTable();
         private static MySqlDataAdapter sda;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static void EstablishConnection()
         {
             try
@@ -31,30 +34,45 @@ namespace RFID_Demo
                 builder.SslMode = MySqlSslMode.None;
 
                 connection = new MySqlConnection(builder.ToString());
-                MessageBox.Show("YES");
+                connection.Open();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("help");
+                MessageBox.Show("Database is disconnected!");
             }
 
         }
-      
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static void UpdateBookTable()
         {
             dt.Clear();
             string query = "Select Book_Title, Book_Autor, Book_Genre, Book_Image, Book_RFID_EPC, Book_RFID_TimeStamp, Book_RFID_RSSI from books";
-            if (connection != null)
+            try
             {
-                connection.Open();
-                cmd = new MySqlCommand(query, connection);
-                da = new MySqlDataAdapter(cmd);
-                da.Fill(dt);
-                connection.Close();
+                if (connection != null)
+                {
+                    connection.Open();
+                    cmd = new MySqlCommand(query, connection);
+                    da = new MySqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    connection.Close();
+                }
             }
-         
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static void addBookQuery(string EPC, string TimeStamp, string RSSI, string Book_Title, string Book_Autor, string Book_Genre, ImageSource source)
         {
             MemoryStream ms = new MemoryStream();
@@ -62,91 +80,78 @@ namespace RFID_Demo
 
             String query = "INSERT INTO books (Book_id, Book_Title, Book_Autor, Book_Genre, Book_Image, Book_RFID_EPC, Book_RFID_TimeStamp, Book_RFID_RSSI) " +
                 "VALUE(@Book_id, @Book_Title, @Book_Autor, @Book_Genre, @Book_Image, @Book_RFID_EPC, @Book_RFID_TimeStamp, @Book_RFID_RSSI)";
-            if (connection != null)
-            {
-                connection.Open();
-                cmd = new MySqlCommand(query, connection);
-
-                cmd.Parameters.Add("@Book_id", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_Title", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_Autor", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_Genre", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_Image", MySqlDbType.Blob);
-                cmd.Parameters.Add("@Book_RFID_EPC", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_RFID_TimeStamp", MySqlDbType.VarChar, 255);
-                cmd.Parameters.Add("@Book_RFID_RSSI", MySqlDbType.VarChar, 255);
-
-
-                cmd.Parameters["@Book_id"].Value = Guid.NewGuid().ToString();
-                cmd.Parameters["@Book_Title"].Value = Book_Title;
-                cmd.Parameters["@Book_Autor"].Value = Book_Autor;
-                cmd.Parameters["@Book_Genre"].Value = Book_Genre;
-                cmd.Parameters["@Book_Image"].Value = source;
-                cmd.Parameters["@Book_RFID_EPC"].Value = EPC;
-                cmd.Parameters["@Book_RFID_TimeStamp"].Value = TimeStamp;
-                cmd.Parameters["@Book_RFID_RSSI"].Value = RSSI;
-
-                if (cmd.ExecuteNonQuery() == 1)
-                {
-                    MessageBox.Show("nice job");
-                }
-                connection.Close();
-                UpdateBookTable();
-            }
-
-        }
-
-        public static void RemoveBookQuery(string EPC) {
-            connection.Open();
-            String query = "DELETE FROM books Where Book_RFID_EPC = '" + EPC+ "'";
-            if (connection != null)
-            {
-                cmd = new MySqlCommand(query, connection);
-
-                if (cmd.ExecuteNonQuery() == 1)
-                {
-                    MessageBox.Show("nice job");
-                }
-                connection.Close();
-                UpdateBookTable();
-            }
-        }
-
-
-        public static DataTable GetDT() {
-            
-            foreach (DataRow dataRow in dt.Rows)
-            {
-                foreach (var item in dataRow.ItemArray)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-            return dt;
-        }
-
-
-        public static MySqlCommand RunQuery(string query)
-        {
             try
             {
                 if (connection != null)
                 {
                     connection.Open();
-                    cmd = connection.CreateCommand();
-                    cmd.CommandType = CommandType.Text;
-                    cmd.CommandText = query;
+                    cmd = new MySqlCommand(query, connection);
 
+                    cmd.Parameters.Add("@Book_id", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_Title", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_Autor", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_Genre", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_Image", MySqlDbType.Blob);
+                    cmd.Parameters.Add("@Book_RFID_EPC", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_RFID_TimeStamp", MySqlDbType.VarChar, 255);
+                    cmd.Parameters.Add("@Book_RFID_RSSI", MySqlDbType.VarChar, 255);
+
+
+                    cmd.Parameters["@Book_id"].Value = Guid.NewGuid().ToString();
+                    cmd.Parameters["@Book_Title"].Value = Book_Title;
+                    cmd.Parameters["@Book_Autor"].Value = Book_Autor;
+                    cmd.Parameters["@Book_Genre"].Value = Book_Genre;
+                    cmd.Parameters["@Book_Image"].Value = source;
+                    cmd.Parameters["@Book_RFID_EPC"].Value = EPC;
+                    cmd.Parameters["@Book_RFID_TimeStamp"].Value = TimeStamp;
+                    cmd.Parameters["@Book_RFID_RSSI"].Value = RSSI;
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("New book add to the data base: \n" +
+                            "Summary \n" +
+                            "");
+                    }
+                    connection.Close();
+                    UpdateBookTable();
                 }
-
-            }
-            catch (Exception ex)
+            }catch(Exception ex)
             {
-                connection.Close();
-                MessageBox.Show("Error");
-            }
-            return cmd;
-        }
-    }
+                MessageBox.Show("Database is disconnected. Can not add item.");
 
+            }
+
+        }
+
+
+        /// <summary>
+        /// /
+        /// </summary>
+        public static void RemoveBookQuery(string EPC)
+        {
+            connection.Open();
+            String query = "DELETE FROM books Where Book_RFID_EPC = '" + EPC + "'";
+            if (connection != null)
+            {
+                cmd = new MySqlCommand(query, connection);
+
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Item with the EPC:"+ EPC +" was removed from the database.");
+                }
+                connection.Close();
+                UpdateBookTable();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GetDT()
+        {
+            return dt;
+        }
+
+    }
 }
--- RFID_Demo/Objects/UnknownRFID.cs
+++ RFID_Demo/Objects/UnknownRFID.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,77 +13,115 @@ using static RFID_Demo.Itembook;
 namespace RFID_Demo
 {
 
-   
+
     public class UnknownRFID
     {
         public string _EPC;
         public string _timeStamp;
         public string _RSSI;
-        
+
         public UnknownRFID(String EPC, String timeStamp, String RSSI)
         {
             this.RSSI = RSSI;
             this.EPC = EPC;
-            this.timeStamp = timeStamp; 
+            this.timeStamp = timeStamp;
 
         }
         public string EPC
-        { 
+        {
             get { return _EPC; }
             set { _EPC = value; }
         }
-        public string timeStamp {
+        public string timeStamp
+        {
             get { return _timeStamp; }
             set { _timeStamp = value; }
-           
+
         }
         public string RSSI
         {
             get { return _RSSI; }
             set { _RSSI = value; }
-         
+
         }
 
         private static ObservableCollection<UnknownRFID> UnknownList = new ObservableCollection<UnknownRFID>();
 
         public class UnknownRFIDList
         {
-          
 
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
             public static ObservableCollection<UnknownRFID> getUnknownRFIDList()
             {
                 return UnknownList;
             }
-        
-            public static void printUnknow() {
+
+
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public static void printUnknow()
+            {
                 for (int i = 0; i < UnknownList.Count; i++)
                 {
                     Console.WriteLine(string.Concat(UnknownList[i].EPC, "---", UnknownList[i].timeStamp));
-    }
-            }
-        public static void addUnknownRFIDItem(String EPC, String timeStamp, String RSSI)
-            {
-        
-                UnknownList.Add(new UnknownRFID(EPC = EPC, timeStamp = timeStamp.ToString(), RSSI = RSSI)) ;
-                 Console.WriteLine(EPC + "   " + timeStamp);
+                }
             }
 
-            public static void RemoveUnknownRFIDItem(UnknownRFID selectedItem) {
-                UnknownList.Remove(UnknownList.Where(i => i.EPC == selectedItem.EPC).Single());
-     
+
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="EPC"></param>
+            /// <param name="timeStamp"></param>
+            /// <param name="RSSI"></param>
+            public static void addUnknownRFIDItem(String EPC, String timeStamp, String RSSI)
+            {
+
+                UnknownList.Add(new UnknownRFID(EPC = EPC, timeStamp = timeStamp.ToString(), RSSI = RSSI));
+                Console.WriteLine(EPC + "   " + timeStamp);
             }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="selectedItem"></param>
+            public static void RemoveUnknownRFIDItem(UnknownRFID selectedItem)
+            {
+                UnknownList.Remove(UnknownList.Where(i => i.EPC == selectedItem.EPC).Single());
+
+            }
+
+
+            /// <summary>
+            /// 
+            /// </summary>
             public static void RemoveALL()
             {
                 UnknownList.Clear();
 
             }
 
-            public static bool CheckList(TagReadDataEventArgs e) {
-                //
-                if (BookListing.getBookList().Any(p => p.EPC == e.TagReadData.EpcString))
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="e"></param>
+            /// <returns></returns>
+            public static bool CheckList(TagReadDataEventArgs e)
+            {
+                //Check dt if EPC already exist. 
+                DataTable dt = DBHelper.GetDT();
+                DataRow[] foundEPC = dt.Select("Book_RFID_EPC = '" + e.TagReadData.EpcString + "'");
+                if (foundEPC.Length != 0)
                 {
                     return false;
                 }
+                 
 
                 if (UnknownList.Any(p => p.EPC == e.TagReadData.EpcString))
                 {
@@ -94,13 +133,13 @@ namespace RFID_Demo
                 }
                 else
                 {
-                   addUnknownRFIDItem(e.TagReadData.EpcString, e.TagReadData.Time.ToString(), e.TagReadData.Rssi.ToString());
+                    addUnknownRFIDItem(e.TagReadData.EpcString, e.TagReadData.Time.ToString(), e.TagReadData.Rssi.ToString());
                     return false;
-                }
                 }
             }
         }
+    }
 
-        }
+}
 
 
